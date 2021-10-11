ff226310e9866b679c33cddc93f1ecdbb3f23ead
廖家圣 <406192065@163.com>
2021-10-11 02:54:52 +0000

    perf: 调整网络

--- iptables.sh
+++ iptables.sh
@@ -3,7 +3,7 @@
 sleep 20s
 /usr/sbin/iptables -I DOCKER-USER -s 172.111.0.0/24 -d 172.111.11.0/24 -j DROP
 /usr/sbin/iptables -I DOCKER-USER -s 172.111.0.0/24 -d 172.111.0.0/24 -j DROP
-/usr/sbin/iptables -I DOCKER-USER -s 172.111.0.0/24 -d 172.111.11.3/32 -j ACCEPT
+/usr/sbin/iptables -I DOCKER-USER -s 172.111.0.0/24 -d 172.111.11.24/32 -j ACCEPT
 
 # webrtc coturn服务 相关端口开放
 firewall-cmd --zone=public --add-port=3478/udp --permanent
